3204ee3876871c573a1d4c1544bc3babbb9025b3
zack10 <zacksmetz@yahoo.com>
2024-03-12 19:47:28 +0100

    movie-page done1

---
+++ collection.js
@@ -0,0 +1,9 @@
+const collection = [
+  {
+    name: "LIFT",
+    Produced: "Gary Gray and Kevin Hart",
+    releaseYear: 2023,
+    genre: ["Comedie", "Action"],
+    cast: ["Kevin Hart", "Jacob Batalon", "Úrsula Corberó"],
+  },
+];
---
+++ main.js
@@ -0,0 +1,3 @@
+const article = document.querySelector("article");
+const article1 = document.querySelector(".article1");
+const main = document.querySelector("main");
